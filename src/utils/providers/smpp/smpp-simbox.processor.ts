64c6ed2f351cc7e5947c88485838ad78a8f17221
Rize Kishimaro <jokerlove2671@gmail.com>
2025-09-12 16:46:24 +0630

    fix: added 3 route

---
+++ src/utils/providers/smpp/smpp-simbox.processor.ts
@@ -0,0 +1,33 @@
+
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import * as smpp from "smpp";
+
+@Injectable()
+export class SmppSimboxProvider implements OnModuleInit {
+  public session: any;
+
+  onModuleInit() {
+    this.session = smpp.connect({ url: process.env.SMPP_URL });
+
+    this.session.bind_transceiver(
+      {
+        system_id: process.env.SMPP_SIMBOX_USERNAME,
+        password: process.env.SMPP_SIMBOX_PASSWORD,
+      },
+      (pdu) => {
+        if (pdu.command_status !== 0) {
+          console.error("❌ SMPP bind failed:", pdu);
+          process.exit(1);
+        } else {
+          console.log("✅ SMPP connected & bound as transceiver");
+        }
+      },
+    );
+
+    // Just emit events — logic handled elsewhere
+    this.session.on("deliver_sm", (pdu) => {
+      this.session.emit("dlr", pdu);
+    });
+  }
+}
+
