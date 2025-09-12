579fb55347b13e60301e448b295f88b32d307458
Rize Kishimaro <jokerlove2671@gmail.com>
2025-09-12 17:04:41 +0630

    fix: added smpp clients

--- src/utils/providers/smpp/smpp.module.ts
+++ src/utils/providers/smpp/smpp.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module } from "@nestjs/common";
 import { SmppProvider } from "./smpp.service";
+import { SmppSimboxProvider } from "./smpp-simbox.processor";
+import { SmppWholesaleProvider } from "./smpp-wholesale.service";
 @Global()
 @Module(
   {
-    providers: [SmppProvider],
-    exports: [SmppProvider]
+    providers: [SmppProvider, SmppWholesaleProvider, SmppSimboxProvider],
+    exports: [SmppProvider, SmppSimboxProvider, SmppWholesaleProvider]
   }
 )
 export class SmppProviderModule {
