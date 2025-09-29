b6141c7e4e75460d4ff93742ee970ee45e98a6ca
Rize Kishimaro <jokerlove2671@gmail.com>
2025-09-30 05:15:49 +0630

    fix: role based filter

--- src/customer-crm/customer-crm.service.ts
+++ src/customer-crm/customer-crm.service.ts
@@ -135,6 +135,9 @@ export class CustomerCrmService {
 
 
   async listLeads(q: QueryDto, req: ExpressRequest) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: req.user.user.id },
+    })
     const baseWhere: Prisma.CRMLeadsWhereInput = q.search
       ? {
         OR: [
@@ -142,6 +145,7 @@ export class CustomerCrmService {
           { phone: { contains: q.search, mode: "insensitive" } },
           { companyName: { contains: q.search, mode: "insensitive" } },
           { address: { contains: q.search, mode: "insensitive" } },
+          user?.roles === 'company_user' ? { systemCompanyId: user.systemCompanyId } : {}
         ],
       }
       : {};
